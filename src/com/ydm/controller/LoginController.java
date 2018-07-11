0a0e2407db8a29a4629e098a857bfa35b9349b3e
ym <ym@DESKTOP-PSL1KM6>
2018-07-11 17:40:05 +0800

    add框架

---
+++ src/com/ydm/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.ydm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	@RequestMapping("/login")
+	public String login() {
+		return "index.html";
+	}
+    
+}
